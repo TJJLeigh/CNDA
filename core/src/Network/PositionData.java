2722e5481893912de40b3837f57cc80945639707
Jack Leigh <t.j.j.l.1997@gmail.com>
2016-01-09 18:47:41 -0500

    Network objects

---
+++ core/src/Network/PositionData.java
@@ -0,0 +1,12 @@
+package Network;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Jack on 2016-01-09.
+ */
+public class PositionData {
+    Vector2 paddle1;
+    Vector2 paddle2;
+    Vector2 ball;
+}
