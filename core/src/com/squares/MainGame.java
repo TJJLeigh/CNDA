35a25a0da02e9a964c3a27477c6a52722e0c3866
Albert Stanica <KiloWalt@gmail.com>
2016-01-09 18:19:06 -0500

    GameStateManager plus 2 states

--- core/src/com/squares/MainGame.java
+++ core/src/com/squares/MainGame.java
@@ -1,19 +1,22 @@
 package com.squares;
 
+import GameState.GameStateManager;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Rectangle;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	ShapeRenderer shapeRenderer;
+	GameStateManager gsm;
 	@Override
 	public void create () {
+		gsm = new GameStateManager();
+		gsm.setState(gsm.MENUSTATE);
 		batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
 		img = new Texture("badlogic.jpg");
@@ -23,12 +26,6 @@ public class MainGame extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(img,50,50);
-		batch.end();
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.rect(20,20,50,50);
-        shapeRenderer.end();
+		gsm.update();
 	}
 }
