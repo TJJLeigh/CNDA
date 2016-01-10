1af3bbbaa5d6a24947714d5e5f3d79b4e5654079
Jack Leigh <t.j.j.l.1997@gmail.com>
2016-01-09 19:42:17 -0500

    Added Pong Client

---
+++ core/src/GameState/PongClient.java
@@ -0,0 +1,28 @@
+package GameState;
+
+
+import com.esotericsoftware.kryonet.Server;
+
+/**
+ * Created by Jack on 2016-01-09.
+ */
+public class PongClient extends GameState{
+    Server server;
+    public PongClient(GameStateManager gsm){
+        super(gsm);
+    }
+    @Override
+    public void init(String[] args) {
+        
+    }
+    @Override
+    public void update() {
+
+    }
+    @Override
+    public void draw() {
+
+    }
+
+
+}
